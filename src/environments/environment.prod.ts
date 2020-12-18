d669cb91c1971e80e27f8952dff4abf0faf68468
Jmiguel14 <miguel.jurado@epn.edu.ec>
2020-12-18 10:47:44 -0500

    All Ok ;)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyD3gOi88m1D8bYqOYJTwEcDj_BSIMM4Qgo",
+    authDomain: "ionic-login-1b955.firebaseapp.com",
+    projectId: "ionic-login-1b955",
+    storageBucket: "ionic-login-1b955.appspot.com",
+    messagingSenderId: "334600281140",
+    appId: "1:334600281140:web:d564b8a4abf5d84ff53e61",
+    measurementId: "G-QSLCXREVVE"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyD3gOi88m1D8bYqOYJTwEcDj_BSIMM4Qgo",
+    authDomain: "ionic-login-1b955.firebaseapp.com",
+    projectId: "ionic-login-1b955",
+    storageBucket: "ionic-login-1b955.appspot.com",
+    messagingSenderId: "334600281140",
+    appId: "1:334600281140:web:d564b8a4abf5d84ff53e61",
+    measurementId: "G-QSLCXREVVE"
+  }
 };
 
 /*
